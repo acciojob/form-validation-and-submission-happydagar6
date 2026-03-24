b50e01b2d3aa9fa29f7696d5caa6dbad48eac059
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-03-24 11:11:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const termsCheckBox = document.getElementById("terms");
+const submitBtn = document.getElementById("submitBtn");
+
+termsCheckBox.addEventListener("change", function () {
+	submitBtn.disabled = !this.checked;
+});
